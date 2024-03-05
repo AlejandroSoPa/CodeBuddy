94cd36f2aa9a144481f07dcdd460adaf0cfc277f
PauR03 <priusnori03@gmail.com>
2024-03-05 16:50:28 +0100

    Nuevas bibliotecas(cors y npm-run-all)

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
