f12f70c68411ac48b47610d0157bee0b44ac2c97
PauR03 <priusnori03@gmail.com>
2024-03-11 21:23:52 +0100

    Empezando con React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './componentsCSS/App.css'
+// import { EjemploComponentes } from './components/EjemploComponentes'
+import { Header, Footer } from './components/ComponentesGlobales.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+    // const props = { inicialTextoH2: "Adiós2", textoH3: "Adiós3" }
+
+
+    return (
+        // Hay que usar <></> para devolver varios elementos, <> </> es lo mismo que <React.Fragment></React.Fragment>
+        <>
+            {
+                /*
+                <EjemploComponentes inicialTextoH2={"Hola2"} textoH3="Hola3">
+                    <i>Hola1</i>
+                </EjemploComponentes>
+    
+                { Otra forma de pasar props }
+                <EjemploComponentes {...props}>
+                    Adiós1
+                </EjemploComponentes>
+                */
+            }
+            <Header />
+            <Footer></Footer>
+        </>
+    )
 }
-
-export default App
---
+++ frontend/src/components/ComponentesGlobales.jsx
@@ -0,0 +1,17 @@
+import "./../componentsCSS/ComponentesGlobales.css"
+
+export function Header() {
+    return (
+        <header>
+            <h1>Header</h1>
+        </header>
+    )
+}
+
+export function Footer() {
+    return (
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+    )
+}
