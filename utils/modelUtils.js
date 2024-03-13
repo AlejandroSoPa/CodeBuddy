55bb11f78c294f6b56a0552023f54fc9e0cfb27e
Alejandro <95918617+AlejandroSoPa@users.noreply.github.com>
2024-03-13 20:10:30 +0100

    Arreglo comun (añadida una condicion)

--- utils/modelUtils.js
+++ utils/modelUtils.js
@@ -2,7 +2,7 @@ export function comprobarUsuarioRegister({ nombreUsuario, email, contrasena, con
     const usuarioNulo = comprobarObjetoNULL({ nombreUsuario, email, contrasena, contrasena2 })
     if (usuarioNulo == false) {
         return false
-    } else if (nombreUsuario.length <= 2 || nombreUsuario.length > 16 || email.length <= 5 || contrasena.length < 8 || contrasena.length > 60 || !/\d/.test(contrasena) || !/[\W_]/.test(contrasena || contrasena != contrasena2)){
+    } else if (nombreUsuario.length <= 2 || nombreUsuario.length > 16 || email.length <= 5 || email.length > 255 || contrasena.length < 8 || contrasena.length > 60 || !/\d/.test(contrasena) || !/[\W_]/.test(contrasena || contrasena != contrasena2)){
         return false
     }
     return true
@@ -21,7 +21,7 @@ export function comprobarUsuarioLogin({ email, contrasena }) {
     const usuarioSinRegistrar = comprobarObjetoNULL({ email, contrasena })
     if (usuarioSinRegistrar == false) {
         return false
-    } else if (email.length <= 5 || contrasena.length < 8 || contrasena.length > 60 || !/\d/.test(contrasena) || !/[\W_]/.test(contrasena)){
+    } else if (email.length <= 5 || email.length > 255 || contrasena.length < 8 || contrasena.length > 60 || !/\d/.test(contrasena) || !/[\W_]/.test(contrasena)){
         return false
     }
     return true
