fedbe6a4aec54fee4bb520733b0db9bc5ecee753
PauR03 <priusnori03@gmail.com>
2024-03-13 11:16:53 +0100

    crear registrar(30%):seguimos con el header pero con avances

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,17 +2,19 @@
 import "./Header.css";
 
 export function Header({ isLogged = false, nombreUsuario }) {
+    if (isLogged) {
+        nombreUsuario = "Pautronix996"
+    }
     return (
         <header>
-            <nav>
-                <a href="/home">
+            <nav className="seccion1">
+                <a href="/home" className="logo">
                     <img src="http://localhost:5173/src/assets/logo_88x88.png" alt="Logo de CodeBuddy" />
                 </a>
-                <a href="/home">
-                    <span className="headerInicioTexto">
-                        <img src="http://localhost:5173/src/assets/casa.svg" alt="Icono de casa" />
-                        Inicio
-                    </span>
+                {/* /home o / en general */}
+                <a href="/home" className="headerInicioTexto">
+                    <img src="http://localhost:5173/src/assets/casa.svg" alt="Icono de casa" />
+                    Inicio
                 </a>
                 {isLogged ?
                     (
@@ -21,15 +23,46 @@ export function Header({ isLogged = false, nombreUsuario }) {
                             {nombreUsuario}
                         </span>
                     ) : (
-                        <a href="/login">
-                            <img src="http://localhost:5173/src/assets/iniciarSesion.svg" alt="Icono de usuario" />
-                            Iniciar sesión
-                        </a>
+                        <>
+                            <a href="/login">
+                                <img src="http://localhost:5173/src/assets/iniciarSesion.svg" alt="Icono de usuario" />
+                                Iniciar sesión
+                            </a>
 
+                            <a href="/register">
+                                <img src="http://localhost:5173/src/assets/registrar.svg" alt="Icono de usuario" />
+                                Registrarse
+                            </a>
+                        </>
                     )
                 }
             </nav>
+            <nav className="seccion2">
+                <input type="text" placeholder="Buscar publicación..." />
+            </nav>
+            <nav className="seccion3">
+                {isLogged ?
+                    (
+                        <>
+                            <a href="/logout">
+                                <img src="http://localhost:5173/src/assets/cerrarSesion.svg" alt="Icono de cerrar sesion" />
+                            </a>
+                            <a href="/configs">
+                                <img src="http://localhost:5173/src/assets/configuracion.svg" alt="Icono de configuraciones" />
+                            </a>
+                        </>
 
+                    ) : null
+                }
+                <div className="tema">
+                    <button id="claro">
+                        <img src="http://localhost:5173/src/assets/sol.svg" alt="Icono de sol modo claro" />
+                    </button>
+                    <button id="oscuro">
+                        <img src="http://localhost:5173/src/assets/luna.svg" alt="Icono de luna modo oscuro" />
+                    </button>
+                </div>
+            </nav>
         </header >
     )
 }
